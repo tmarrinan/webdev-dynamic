ccef98c47a57fee53486a767da01c12cea7352e8
Josh Kahlbaugh <josh@secretlab.com>
2023-10-21 22:47:07 -0500

    adding intro and template pages

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -14,6 +14,61 @@ const template = path.join(__dirname, 'templates');
 let app = express();
 app.use(express.static(root));
 
+
+const db = new sqlite3.Database(
+  path.join(__dirname, "equality.sqlite3"),
+  sqlite3.OPEN_READONLY,
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully connected to database");
+    }
+  }
+)
+
+function dbSelect(query, params) {
+  let p = new Promise((resolve, reject) => {
+    db.all(query, params, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log(rows);
+        resolve(rows);
+      }
+    });
+  });
+  return p;
+}
+
+app.get("/all", (req, res) => {
+  console.log("trying!");
+  let p1 = dbSelect("SELECT * FROM Equality");
+  let p2 = fs.promises.readFile(path.join(template, "search.html"), "utf-8");
+  Promise.all([p1, p2]).then((results) => {
+    let equality_list = results[0];
+    let response = results[1];
+    let table_body = "";
+    equality_list.forEach((equality) => {
+      let table_row = "<tr>";
+      table_row += `<td>${equality.country}</td>`;
+      table_row += `<td>${equality.wei}</td>`;
+      table_row += `<td>${equality.weg}</td>`;
+      table_row += `<td>${equality.ggpi}</td>`;
+      table_row += `<td>${equality.ggpg}</td>`;
+      table_row += `<td>${equality.hdg}</td>`;
+      table_row += `<td>${equality.region}</td>`;
+      table_row += "</tr>";
+      table_body += table_row;
+    });
+    response = response.replace("$TABLE_DATA$", table_body);
+      res.status(200).type("html").send(response);
+    
+  }).catch((error) => {
+      res.status(404).type("txt").send(error);
+  });
+})
+
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
 });
