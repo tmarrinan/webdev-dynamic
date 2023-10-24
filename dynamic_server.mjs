1075b435dabf31eb4387dff32003c7cf7a50723f
line7313 <line7313@stthomas.edu>
2023-10-24 12:45:30 -0500

    templating and database connect

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -5,14 +5,33 @@ import * as url from 'node:url';
 import { default as express } from 'express';
 import { default as sqlite3 } from 'sqlite3';
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-
 const port = 8000;
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const root = path.join(__dirname, 'public');
 const template = path.join(__dirname, 'templates');
 
 let app = express();
 app.use(express.static(root));
+
+const db = new sqlite3.Database(path.join(__dirname, 'nhl_data.sqlite3'), sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+        console.log('Error connecting to database');
+    }
+    else {
+        console.log('Successfully connected to database');
+    }
+});
+
+app.get('/', (req, res) => {   
+    console.log("HERE");
+    fs.readFile(path.join(template, 'temp.html'), 'utf-8', (err, data) => {
+        if (err) {
+            res.status(400);
+        }
+        res.status(200).type('html').send(data);
+    })
+    
+});
 
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
