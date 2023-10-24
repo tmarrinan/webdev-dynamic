73e10149940d9514c77037d58ce167d8b528c059
line7313 <line7313@stthomas.edu>
2023-10-24 16:58:45 -0500

    endpoint work

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -8,7 +8,7 @@ import { default as sqlite3 } from 'sqlite3';
 const port = 8000;
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const root = path.join(__dirname, 'public');
-const template = path.join(__dirname, 'templates');
+const templatePath = path.join(__dirname, 'templates');
 
 let app = express();
 app.use(express.static(root));
@@ -22,32 +22,104 @@ const db = new sqlite3.Database(path.join(__dirname, 'nhl_data.sqlite3'), sqlite
     }
 });
 
-app.get('/', (req, res) => {   
-    console.log("HERE");
-    fs.readFile(path.join(template, 'temp.html'), 'utf-8', (err, data) => {
+let template = new Promise((resolve, reject) => {
+    fs.readFile(path.join(templatePath, 'temp.html'), 'utf-8', (err, data) => {
         if (err) {
-            res.status(400);
+            reject(err);
+        } else {
+            resolve(data);
+        }            
+    })
+});
+
+function queryDatabase(column, modifier, queryOverride = false) {
+    return new Promise((resolve, reject) => {
+        let query;
+        if (queryOverride) {
+            query = `SELECT * FROM nhl_data WHERE ${column} ${modifier}`
+        } else {
+            query = `SELECT * FROM nhl_data WHERE ${column} = ${modifier}`
         }
-        res.status(200).type('html').send("<h1> Title Page <h1>");
+
+        console.log(query);
+        db.all(query, (err, rows) => {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                resolve(rows);
+            }
+        })
     })
     
+}
+
+function abbreviationMapper(abbr, column) {
+
+    let queryModifier;
+
+    switch (abbr) {
+        case "verylow":
+            queryModifier = "< 20;"
+            break;
+
+        case "low":
+            queryModifier = `>= 20 AND ${column} < 40;`
+            break;
+
+        case "medium":
+            queryModifier = `>= 40 AND ${column} < 60;`
+            break;
+            
+        case "high":
+            queryModifier = `>= 60 AND ${column} < 80;`
+            break;
+        case "veryhigh":
+            queryModifier = ">= 80;"
+            break;
+        
+        default:
+            queryModifier = ">0" //return all
+    };
+
+    return queryModifier;
+}
+
+
+
+app.get('/', async (req, res) => {   
+    template.then((template) => {
+        res.status(200).type('html').send(template);
+    });     
 });
 
 app.get('/team/:team', (req, res) => {   
     let teamAbbr = req.params.team.toUpperCase();
-    console.log(teamAbbr);
-    
-    let query = `SELECT * FROM nhl_data WHERE home_team_abbr = '${teamAbbr}';`;
-    db.all(query, (err, rows) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log(rows);
-        }
-    })
-    
+    queryDatabase('home_team_abbr', teamAbbr);    
 });
+
+app.get('/quality/:quality', async (req, res) => {   
+    let column = "game_quality_rating";
+    let queryModifier = abbreviationMapper(req.params.quality.toLowerCase(), column);
+    let rows = queryDatabase(column, queryModifier, true);
+    rows.then(() => console.log(rows));
+    template.then((template) => {
+        res.status(200).type('html').send(template);
+    });   
+});
+
+app.get('/importance/:importance', async (req, res) => {   
+    let column = "game_importance_rating";
+    let queryModifier = abbreviationMapper(req.params.importance.toLowerCase(), column);
+    let rows = queryDatabase(column, queryModifier, true);
+    rows.then(() => console.log(rows));
+    template.then((template) => {
+        res.status(200).type('html').send(template);
+    });   
+});
+
+
 
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
