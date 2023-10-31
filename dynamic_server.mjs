eccc2cca43820eb971df01c3e9f8cc196f6b2ab2
line7313 <line7313@stthomas.edu>
2023-10-30 19:10:07 -0500

    Dynamic titles for routes

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -5,11 +5,12 @@ import * as url from 'node:url';
 import { default as express } from 'express';
 import { default as sqlite3 } from 'sqlite3';
 
-const port = 8000;
+const port = 8080;
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const root = path.join(__dirname, 'public');
 const templatePath = path.join(__dirname, 'templates');
 let renderedTemplate = "";
+let titleAbbr = "";
 
 let app = express();
 app.use(express.static(root));
@@ -37,11 +38,11 @@ let template = new Promise((resolve, reject) => {
 function queryDatabase(column, modifier, queryOverride = false) {
     return new Promise((resolve, reject) => {
         let query;
-        let selectionModifier = "season, date, home_team, away_team, home_team_abbr, away_team_abbr, home_team_score, away_team_score, game_quality_rating, game_importance_rating FROM nhl_data WHERE home_team_abbr";
+        let selectionModifier = "season, date, home_team, away_team, home_team_abbr, away_team_abbr, home_team_score, away_team_score, game_quality_rating, game_importance_rating FROM nhl_data WHERE ";
         if (queryOverride) {
-            query = `SELECT ${selectionModifier} FROM nhl_data WHERE ${column} ${modifier};`
+            query = `SELECT ${selectionModifier} ${column} ${modifier};`
         } else {
-            query = `SELECT ${selectionModifier} = '${column}' OR away_team_abbr = '${column}';`
+            query = `SELECT ${selectionModifier} home_team_abbr = '${column}' OR away_team_abbr = '${column}';`
         }
 
         db.all(query, (err, rows) => {
@@ -62,21 +63,26 @@ function abbreviationMapper(abbr, column) {
     switch (abbr) {
         case "verylow":
             queryModifier = "< 20;"
+            titleAbbr = "Very High"
             break;
 
         case "low":
             queryModifier = `>= 20 AND ${column} < 40;`
+            titleAbbr = "Low";
             break;
 
         case "medium":
             queryModifier = `>= 40 AND ${column} < 60;`
+            titleAbbr = "Medium";
             break;
             
         case "high":
             queryModifier = `>= 60 AND ${column} < 80;`
+            titleAbbr = "High";
             break;
         case "veryhigh":
             queryModifier = ">= 80;"
+            titleAbbr = "Very High";
             break;
         
         default:
@@ -86,12 +92,23 @@ function abbreviationMapper(abbr, column) {
     return queryModifier;
 }
 
-function renderTemplate(route, data) {
+
+function renderTemplate(route, data, userInput) {
 
     return new Promise((resolve, reject) => {
         template.then((template) => {
             if (route == "team") {
-                console.log(data);
+                let teamName = mapName(userInput, data);
+                title = `Game Data for ${teamName}`;
+                renderedTemplate = template.replace('##TITLE##', title);
+                resolve(renderedTemplate);
+
+            } else if (route == "quality") {
+                title = `Showing Data For ${titleAbbr} Quality Games `;
+                renderedTemplate = template.replace('##TITLE##', title);
+                resolve(renderedTemplate);
+            } else if (route == "importance") {
+                title = `Showing Data For ${titleAbbr} Importance Games `;
                 renderedTemplate = template.replace('##TITLE##', title);
                 resolve(renderedTemplate);
             }
@@ -103,14 +120,15 @@ function renderTemplate(route, data) {
     
 }
 
-function mapName(inputAbbr, data) {
+function mapName(inputAbbr, data) { 
 
     if (inputAbbr == data[0].home_team_abbr) {
-        title = data[0].home_team;
+         return data[0].home_team;
     } else {
-        title = data[0].away_team;
+        return data[0].away_team;
     }
 }
+
 
 app.get('/', async (req, res) => {   
     template.then((template) => {
@@ -122,8 +140,7 @@ app.get('/team/:team', (req, res) => {
     let teamAbbr = req.params.team.toUpperCase();
     let data = queryDatabase(teamAbbr);
     data.then((data) => {
-        mapName(teamAbbr, data);
-        renderTemplate("team", data).then(() => {
+        renderTemplate("team", data, teamAbbr).then(() => {
         res.status(200).type('html').send(renderedTemplate);         
         });
     });
@@ -132,26 +149,28 @@ app.get('/team/:team', (req, res) => {
 
 app.get('/quality/:quality', async (req, res) => {   
     let column = "game_quality_rating";
-    let queryModifier = abbreviationMapper(req.params.quality.toLowerCase(), column);
+    let quality = req.params.quality.toLowerCase();
+    let queryModifier = abbreviationMapper(quality, column);
     let rows = queryDatabase(column, queryModifier, true);
-    rows.then(() => console.log(rows));
-    renderTemplate("team");
-    template.then((template) => {
-        res.status(200).type('html').send(template);
-    });   
+    rows.then(() => {
+        renderTemplate("quality", rows, quality).then(() => {
+            res.status(200).type('html').send(renderedTemplate);         
+        });
+    });    
 });
 
 app.get('/importance/:importance', async (req, res) => {   
     let column = "game_importance_rating";
-    let queryModifier = abbreviationMapper(req.params.importance.toLowerCase(), column);
+    let importance = req.params.importance.toLowerCase();
+    let queryModifier = abbreviationMapper(importance, column);
     let rows = queryDatabase(column, queryModifier, true);
-    rows.then(() => console.log(rows));
-    renderTemplate("team");
-    template.then((template) => {
-        res.status(200).type('html').send(template);
-    });
+    rows.then(() => {
+        renderTemplate("importance", rows, importance).then(() => {
+            res.status(200).type('html').send(renderedTemplate);         
+        });
+    }); 
 });
 
 app.listen(port, () => {
-    console.log('http://localhost:8000');
+    console.log('http://localhost:8080');
 });
