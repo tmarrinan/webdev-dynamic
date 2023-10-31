51f6e11160eba6f6b2e4d9352ba306a109be05d6
Jonny DeMarais <dema5892@stthomas.edu>
2023-10-31 14:38:53 -0500

    Adding table

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -39,13 +39,60 @@ function dbSelect(query, params) {
 
 app.get('/State/:name', (req, res) => {
 
-    let state = req.params.name.toUpperCase();
+    let state = req.params.name;
 
     let p1 = dbSelect("SELECT * FROM drivers WHERE State=?", [state]);
     let p2 = p1;
     let p3 = fs.promises.readFile(path.join(template, 'template1.html'), 'utf-8');
     Promise.all([p1, p2, p3]).then((results) => {
-        let response = results[2].replace('$$STATE_NAME$$', results[1][0].State);
+        console.log(p1)
+        console.log(results[1][0].State)
+        let response = results[2].replace('$$STATENAME$$', results[1][0].State);
+        res.status(200).type('html').send(response);
+    }).catch((error) => {
+        console.error("Error:", error); // Log the error for debugging
+        res.status(404).type('txt').send('File not found !!!');
+    });
+});
+
+
+app.get('/Insurance/:frequency', (req, res) => { //Car insurance 
+
+    let frequency = req.params.frequency;
+
+    let p1 = dbSelect("SELECT 'State', 'Car Insurance Premiums ($)' FROM drivers WHERE 'Car Insurance Premiums ($)' >?", [frequency]);
+    let p2 = p1;
+    let p3 = fs.promises.readFile(path.join(template, 'template1.html'), 'utf-8');
+    Promise.all([p1, p2, p3]).then((results) => {
+        let response = results[2].replace('$$MFR_NAME$$', results[1][0].name);
+        let table_body = '';
+        results[0].forEach((State) => {
+            let table_row = '<tr>';
+            table_row += '<td>' + State + '</td>';
+            table_row += '</tr>';
+            table_body += table_row;
+        });
+        response = response.replace('$$TABLE_BODY$$', table_body);
+        console.log(p1)
+        res.status(200).type('html').send(response);
+    }).catch((error) => {
+        console.error("Error:", error); // Log the error for debugging
+        res.status(404).type('txt').send('File not found !!!');
+    });
+});
+
+
+app.get('/State/:name', (req, res) => {
+
+    let state = req.params.name;
+
+    let p1 = dbSelect("SELECT * FROM drivers WHERE State=?", [state]);
+    let p2 = p1;
+    let p3 = fs.promises.readFile(path.join(template, 'template1.html'), 'utf-8');
+    Promise.all([p1, p2, p3]).then((results) => {
+        console.log(p1)
+        console.log(results[1][0].State)
+        let response = results[2].replace('$$STATENAME$$', results[1][0].State);
         res.status(200).type('html').send(response);
     }).catch((error) => {
         console.error("Error:", error); // Log the error for debugging
