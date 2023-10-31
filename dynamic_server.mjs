9a581d0c8efd2acd56d30bf8e9bf32764474b70c
mucho <much2718@stthomas.edu>
2023-10-31 16:08:58 -0500

    Edits on first two paths of the website

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -42,12 +42,19 @@ app.get('/State/:name', (req, res) => {
     let state = req.params.name;
 
     let p1 = dbSelect("SELECT * FROM drivers WHERE State=?", [state]);
-    let p2 = p1;
-    let p3 = fs.promises.readFile(path.join(template, 'template1.html'), 'utf-8');
-    Promise.all([p1, p2, p3]).then((results) => {
+    let p2 = fs.promises.readFile(path.join(template, 'template2.html'), 'utf-8');
+    Promise.all([p1, p2]).then((results) => {
         console.log(p1)
-        console.log(results[1][0].State)
-        let response = results[2].replace('$$STATENAME$$', results[1][0].State);
+        let response = results[1].replace('$$STATENAME$$', results[0][0].State);
+        results[0].forEach((row) => {
+            response = response.replace('$$INPUT1$$', row["Number of drivers involved in fatal collisions per billion miles"]);
+            response = response.replace('$$INPUT2$$', row["Percentage Of Drivers Involved In Fatal Collisions Who Were Speeding"]);
+            response = response.replace('$$INPUT3$$', row["Percentage Of Drivers Involved In Fatal Collisions Who Were Alcohol-Impaired"]);
+            response = response.replace('$$INPUT4$$', row["Percentage Of Drivers Involved In Fatal Collisions Who Were Not Distracted"]);
+            response = response.replace('$$INPUT5$$', row["Percentage Of Drivers Involved In Fatal Collisions Who Had Not Been Involved In Any Previous Accidents"]);
+            response = response.replace('$$INPUT6$$', row["Car Insurance Premiums ($)"]);
+            response = response.replace('$$INPUT7$$', row["Losses incurred by insurance companies for collisions per insured driver ($)"]);
+        });
         res.status(200).type('html').send(response);
     }).catch((error) => {
         console.error("Error:", error); // Log the error for debugging
@@ -60,20 +67,27 @@ app.get('/Insurance/:frequency', (req, res) => { //Car insurance
 
     let frequency = req.params.frequency;
 
-    let p1 = dbSelect("SELECT 'State', 'Car Insurance Premiums ($)' FROM drivers WHERE 'Car Insurance Premiums ($)' >?", [frequency]);
-    let p2 = p1;
-    let p3 = fs.promises.readFile(path.join(template, 'template1.html'), 'utf-8');
-    Promise.all([p1, p2, p3]).then((results) => {
-        let response = results[2].replace('$$MFR_NAME$$', results[1][0].name);
+    if ((frequency % 100) >= 50) {
+        frequency = frequency - ((frequency % 100)) + 100
+    }
+    else {
+        frequency = frequency - ((frequency % 100))
+    }
+
+
+    let p1 = dbSelect("SELECT State, [Car Insurance Premiums ($)] FROM drivers WHERE [Car Insurance Premiums ($)] > ?", [frequency]);
+    let p2 = fs.promises.readFile(path.join(template, 'template1.html'), 'utf-8');
+    Promise.all([p1, p2]).then((results) => {
+        let response = results[1].replace('$$STATENAME$$', 'States with Insurance Premiums over ' + frequency + " (URL Rounded to Nearest 100)");
         let table_body = '';
-        results[0].forEach((State) => {
+        results[0].forEach((row) => {
             let table_row = '<tr>';
-            table_row += '<td>' + State + '</td>';
+            table_row += '<td>' + row.State + '</td>';
+            table_row += '<td>' + row["Car Insurance Premiums ($)"] + '</td>';
             table_row += '</tr>';
             table_body += table_row;
         });
         response = response.replace('$$TABLE_BODY$$', table_body);
-        console.log(p1)
         res.status(200).type('html').send(response);
     }).catch((error) => {
         console.error("Error:", error); // Log the error for debugging
