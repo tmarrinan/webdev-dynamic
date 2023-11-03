da518d48c0dc8fcaa5eecc617e5272042ca92656
Noah Kruse <Krus7615@stthomas.edu>
2023-11-03 12:52:13 -0500

    fixed buttons graph is makeshift without names.

--- server.mjs
+++ server.mjs
@@ -2,42 +2,43 @@ import * as fs from 'node:fs'
 import * as path from 'node:path';
 import * as http from 'node:http';
 import * as url from 'node:url';
-import {default as express} from 'express';
-import {default as sqlite3} from 'sqlite3';
+import { default as express } from 'express';
+import { default as sqlite3 } from 'sqlite3';
 
-const port=8000;
-const __dirname= path.dirname(url.fileURLToPath(import.meta.url));
-const root=path.join(__dirname,'public');
-const template = path.join(__dirname,'templates');
+const port = 8000;
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const root = path.join(__dirname, 'public');
+const template = path.join(__dirname, 'templates');
 
-let app=express();
+let app = express();
 app.use(express.static(root));
 
-const db = new sqlite3.Database(path.join(__dirname,'candy.sqlite3'),sqlite3.OPEN_READONLY, (err)=>{
-    if(err){
+const db = new sqlite3.Database(path.join(__dirname, 'candy.sqlite3'), sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
         console.log('Error connecting to database');
     }
-    else{
+    else {
         console.log('Successfully connected to database');
-    }});
+    }
+});
 
-function dbSelect(query,params){
-    let p = new Promise((resolve, reject)=>{
-        db.all(query, params, (err,rows)=>{
-            if (err){
+function dbSelect(query, params) {
+    let p = new Promise((resolve, reject) => {
+        db.all(query, params, (err, rows) => {
+            if (err) {
                 reject(err);
             }
-            else{
+            else {
                 resolve(rows);
             }
         });
     });
     return p;
-}   
+}
 
-let query1="SELECT * FROM candy WHERE sugarpercent < 0.25 ORDER BY sugarpercent"
-let query2="SELECT * FROM candy WHERE chocolate == 1 AND winpercent > 0.7 ORDER BY winpercent DESC"
-let query3="SELECT * FROM candy WHERE fruity == 1 AND winpercent > 0.7 ORDER BY winpercent DESC"
+let query1 = "SELECT * FROM candy WHERE sugarpercent < 0.25 ORDER BY sugarpercent"
+let query2 = "SELECT * FROM candy WHERE chocolate == 1 AND winpercent > 0.7 ORDER BY winpercent DESC"
+let query3 = "SELECT * FROM candy WHERE fruity == 1 AND winpercent > 0.7 ORDER BY winpercent DESC"
 
 app.get('/', (req, res) => {
     // Send the index.html file when the homepage is accessed
@@ -52,12 +53,12 @@ function convertToYesNo(value) {
 //for making candy names able to be read as fils
 function slugify(text) {
     return text.toString().toLowerCase()
-      .replace(/\s+/g, '-')           // Replace spaces with -
-      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-      .trim();                        // Trim - from start of text
-  }
-  
+        .replaceAll(/\s+/g, '-')           // Replace spaces with -
+        .replaceAll(/[^\w\-]+/g, '')       // Remove all non-word chars
+        .replaceAll(/\-\-+/g, '-')         // Replace multiple - with single -
+        .trim();                        // Trim - from start of text
+}
+
 
 //SUGAR PERCENT
 app.get('/sugarpercent/:category', (req, res) => {
@@ -65,7 +66,7 @@ app.get('/sugarpercent/:category', (req, res) => {
     let category = req.params.category;
     let sugarMin;
     let sugarMax;
-    
+
     switch (category) {
         case 'low':
             sugarMin = 0;
@@ -86,25 +87,29 @@ app.get('/sugarpercent/:category', (req, res) => {
             break;
     }
     // You don't need to perform a query with the candy name here since your queries don't use it.
-    let p1 = dbSelect('SELECT * FROM candy WHERE sugarpercent < ? AND sugarpercent >= ? ORDER BY sugarpercent',[sugarMax,sugarMin]);
+    let p1 = dbSelect('SELECT * FROM candy WHERE sugarpercent < ? AND sugarpercent >= ? ORDER BY sugarpercent', [sugarMax, sugarMin]);
     let p2 = fs.promises.readFile(path.join(template, 'sugarpercent.html'), 'utf-8');
 
     Promise.all([p1, p2]).then(results => {
         // This is where you read the template and insert the data
         let template = results[1]; // This is your HTML template as a string.
-        let response = template.replace('$$CATEGORY_NAME$$', category); // Replace placeholder with candy name from URL.
-        
+        let response = template.replaceAll('$$CATEGORY_NAME$$', category); // Replace placeholder with candy name from URL.
+        let nameList = []
+        let namesList = []
+        let percList = []
         // Now, insert the data from the database into the template.
         // Assuming you want to display the results from p1 in a table.
-        let table_body = results[0].map((candy,index) => { // Use map to transform each row into an HTML row.
+        let table_body = results[0].map((candy, index) => { // Use map to transform each row into an HTML row.
             const imageName = slugify(candy.competitorname) + '.jpg';
             const imagePath = `/images/${imageName}`;
+            nameList.push(candy.competitorname);
+            percList.push(parseFloat(candy.sugarpercent).toFixed(2))
             const imageTag = index === 0 ? `<img src="${imagePath}" alt="Image of ${candy.competitorname}" style="max-width:100px;max-height:100px;">` : '';
-            response = response.replace('$$FEATURED_IMAGE$$', imageTag);
-            response = response.replace('$$IMAGE_CAPTION$$', candy.competitorname);
+            response = response.replaceAll('$$FEATURED_IMAGE$$', imageTag);
+            response = response.replaceAll('$$IMAGE_CAPTION$$', candy.competitorname);
             return `<tr>
                 <td>${candy.competitorname}</td>
-                <td>${parseFloat(candy.sugarpercent).toFixed(2)+'%'}</td>
+                <td>${parseFloat(candy.sugarpercent).toFixed(2) + '%'}</td>
                 <td>${convertToYesNo(candy.chocolate)}</td>
                 <td>${convertToYesNo(candy.fruity)}</td>
                 <td>${convertToYesNo(candy.peanutyalmondy)}</td>
@@ -112,8 +117,13 @@ app.get('/sugarpercent/:category', (req, res) => {
                 <td>${convertToYesNo(candy.pluribus)}</td>
             </tr>`;
         }).join(''); // Join all the strings into one big string.
-        response = response.replace('$$TABLE_BODY$$', table_body); // Replace the table body placeholder.
-      //  response = response.replace('$$FEATURED_IMAGE$$', imageTag);
+        for (let i = 1; i < nameList.length; i++) {
+            namesList.push(i)
+        }
+        response = response.replaceAll('$$NAMES$$', namesList);
+        response = response.replaceAll('$$VALUES$$', percList);
+        response = response.replaceAll('$$TABLE_BODY$$', table_body); // Replace the table body placeholder.
+        //  response = response.replace('$$FEATURED_IMAGE$$', imageTag);
 
         res.status(200).type('html').send(response); // Send the response.
     }).catch((error) => {
@@ -129,7 +139,7 @@ app.get('/winpercent/:category', (req, res) => {
     let category = req.params.category;
     let winMin;
     let winMax;
-    
+
     switch (category) {
         case 'low':
             winMin = 0;
@@ -150,28 +160,28 @@ app.get('/winpercent/:category', (req, res) => {
             break;
     }
     // You don't need to perform a query with the candy name here since your queries don't use it.
-    let p1 = dbSelect('SELECT * FROM candy WHERE winpercent < ? AND winpercent >= ? ORDER BY winpercent DESC',[winMax,winMin]);
+    let p1 = dbSelect('SELECT * FROM candy WHERE winpercent < ? AND winpercent >= ? ORDER BY winpercent DESC', [winMax, winMin]);
     let p2 = fs.promises.readFile(path.join(template, 'winpercent.html'), 'utf-8');
 
     Promise.all([p1, p2]).then(results => {
         // This is where you read the template and insert the data
         let template = results[1]; // This is your HTML template as a string.
-        let response = template.replace('$$CATEGORY_NAME$$', category); // Replace placeholder with candy name from URL.
+        let response = template.replaceAll('$$CATEGORY_NAME$$', category); // Replace placeholder with candy name from URL.
         let nameList = []
         let namesList = []
         let percList = []
         // Now, insert the data from the database into the template.
         // Assuming you want to display the results from p1 in a table.
-        let table_body = results[0].map((candy,index) => { // Use map to transform each row into an HTML row.
+        let table_body = results[0].map((candy, index) => { // Use map to transform each row into an HTML row.
             const imageName = slugify(candy.competitorname) + '.jpg';
             const imagePath = `/images/${imageName}`;
             nameList.push(candy.competitorname);
             percList.push(parseFloat(candy.winpercent).toFixed(1))
             const imageTag = index === 0 ? `<img src="${imagePath}" alt="${imageName}" style="max-width:100px;max-height:100px;">` : '';
-            response = response.replace('$$FEATURED_IMAGE$$', imageTag);
+            response = response.replaceAll('$$FEATURED_IMAGE$$', imageTag);
             return `<tr>
                 <td>${candy.competitorname}</td>
-                <td>${parseFloat(candy.winpercent).toFixed(1)+'%'}</td>
+                <td>${parseFloat(candy.winpercent).toFixed(1) + '%'}</td>
                 <td>${convertToYesNo(candy.chocolate)}</td>
                 <td>${convertToYesNo(candy.fruity)}</td>
                 <td>${convertToYesNo(candy.peanutyalmondy)}</td>
@@ -179,13 +189,13 @@ app.get('/winpercent/:category', (req, res) => {
                 <td>${convertToYesNo(candy.pluribus)}</td>
             </tr>`;
         }).join(''); // Join all the strings into one big string.
-        for(let i=1; i<nameList.length;i++){
+        for (let i = 1; i < nameList.length; i++) {
             namesList.push(i)
         }
         response = response.replaceAll('$$NAMES$$', namesList);
         response = response.replaceAll('$$VALUES$$', percList);
         response = response.replaceAll('$$TABLE_BODY$$', table_body); // Replace the table body placeholder.
-      //  response = response.replace('$$FEATURED_IMAGE$$', imageTag);
+        //  response = response.replace('$$FEATURED_IMAGE$$', imageTag);
         res.status(200).type('html').send(response); // Send the response.
     }).catch((error) => {
         console.error(error);
@@ -200,7 +210,7 @@ app.get('/pricepercent/:category', (req, res) => {
     let category = req.params.category;
     let priceMin;
     let priceMax;
-    
+
     switch (category) {
         case 'low':
             priceMin = 0;
@@ -221,24 +231,28 @@ app.get('/pricepercent/:category', (req, res) => {
             break;
     }
     // You don't need to perform a query with the candy name here since your queries don't use it.
-    let p1 = dbSelect('SELECT * FROM candy WHERE pricepercent < ? AND pricepercent >= ? ORDER BY pricepercent',[priceMax,priceMin]);
+    let p1 = dbSelect('SELECT * FROM candy WHERE pricepercent < ? AND pricepercent >= ? ORDER BY pricepercent', [priceMax, priceMin]);
     let p2 = fs.promises.readFile(path.join(template, 'pricepercent.html'), 'utf-8');
 
     Promise.all([p1, p2]).then(results => {
         // This is where you read the template and insert the data
         let template = results[1]; // This is your HTML template as a string.
-        let response = template.replace('$$CATEGORY_NAME$$', category); // Replace placeholder with candy name from URL.
-        
+        let response = template.replaceAll('$$CATEGORY_NAME$$', category); // Replace placeholder with candy name from URL.
+        let nameList = []
+        let namesList = []
+        let percList = []
         // Now, insert the data from the database into the template.
         // Assuming you want to display the results from p1 in a table.
-        let table_body = results[0].map((candy,index) => { // Use map to transform each row into an HTML row.
+        let table_body = results[0].map((candy, index) => { // Use map to transform each row into an HTML row.
             const imageName = slugify(candy.competitorname) + '.jpg';
             const imagePath = `/images/${imageName}`;
+            nameList.push(candy.competitorname);
+            percList.push(parseFloat(candy.pricepercent).toFixed(2))
             const imageTag = index === 0 ? `<img src="${imagePath}" alt="${imageName}" style="max-width:100px;max-height:100px;">` : '';
-            response = response.replace('$$FEATURED_IMAGE$$', imageTag);
+            response = response.replaceAll('$$FEATURED_IMAGE$$', imageTag);
             return `<tr>
                 <td>${candy.competitorname}</td>
-                <td>${parseFloat(candy.pricepercent).toFixed(2)+'%'}</td>
+                <td>${parseFloat(candy.pricepercent).toFixed(2) + '%'}</td>
                 <td>${convertToYesNo(candy.chocolate)}</td>
                 <td>${convertToYesNo(candy.fruity)}</td>
                 <td>${convertToYesNo(candy.peanutyalmondy)}</td>
@@ -246,8 +260,13 @@ app.get('/pricepercent/:category', (req, res) => {
                 <td>${convertToYesNo(candy.pluribus)}</td>
             </tr>`;
         }).join(''); // Join all the strings into one big string.
-        response = response.replace('$$TABLE_BODY$$', table_body); // Replace the table body placeholder.
-      //  response = response.replace('$$FEATURED_IMAGE$$', imageTag);
+        for (let i = 1; i < nameList.length; i++) {
+            namesList.push(i)
+        }
+        response = response.replaceAll('$$NAMES$$', namesList);
+        response = response.replaceAll('$$VALUES$$', percList);
+        response = response.replaceAll('$$TABLE_BODY$$', table_body); // Replace the table body placeholder.
+        //  response = response.replace('$$FEATURED_IMAGE$$', imageTag);
         res.status(200).type('html').send(response); // Send the response.
     }).catch((error) => {
         console.error(error);
@@ -255,52 +274,52 @@ app.get('/pricepercent/:category', (req, res) => {
     });
 });
 
-db.all(query1,(err,rows)=>{
-    if ((err)=>{
+db.all(query1, (err, rows) => {
+    if ((err) => {
         console.log(err)
     });
-    else{            //res.json(rows)
+    else {            //res.json(rows)
         // rows.forEach(row => console.log(`${row.mfr} is ${row.name} `))
-        data1=rows;
-        if(data1 !==null && data2 !==null && data3 !==null){
+        data1 = rows;
+        if (data1 !== null && data2 !== null && data3 !== null) {
             finishAndSend();
         }
     };
 });
 
-db.all(query2,(err,rows)=>{
-    if ((err)=>{
+db.all(query2, (err, rows) => {
+    if ((err) => {
         console.log(err)
     });
-    else{            //res.json(rows)
+    else {            //res.json(rows)
         // rows.forEach(row => console.log(`${row.mfr} is ${row.name} `))
-        data2=rows;
-        if(data1 !==null && data2 !==null && data3 !==null){
+        data2 = rows;
+        if (data1 !== null && data2 !== null && data3 !== null) {
             finishAndSend();
         }
     };
 });
 
-db.all(query3,(err,rows)=>{
-    if ((err)=>{
+db.all(query3, (err, rows) => {
+    if ((err) => {
         console.log(err)
     });
-    else{            //res.json(rows)
+    else {            //res.json(rows)
         // rows.forEach(row => console.log(`${row.mfr} is ${row.name} `))
-        data3=rows;
-        if(data1 !==null && data2 !==null && data3 !==null){
+        data3 = rows;
+        if (data1 !== null && data2 !== null && data3 !== null) {
             finishAndSend();
         }
     };
 });
 
- 
 
-app.listen(port, () =>{
-    console.log('Now listening on port'+port);
+
+app.listen(port, () => {
+    console.log('Now listening on port' + port);
 });
 
-function graphDraw(names, values){
+function graphDraw(names, values) {
 
     var graphtrace = {
         type: 'scatter',
@@ -308,57 +327,57 @@ function graphDraw(names, values){
         y: values,
         mode: 'markers',
         name: '',
-        marker:{
+        marker: {
             color: 'rgb(255, 0, 0)',
-            line:{
+            line: {
                 color: 'rgb(0,0,255)',
-                width:1,
+                width: 1,
             },
-            symbol:'circle',
-            size:16
+            symbol: 'circle',
+            size: 16
         }
     };
 
     var layout = {
         title: 'Candy Graph',
         xaxis: {
-          showgrid: false,
-          showline: true,
-          linecolor: 'rgb(102, 102, 102)',
-          titlefont: {
-            font: {
-              color: 'rgb(204, 204, 204)'
-            }
-          },
-          tickfont: {
-            font: {
-              color: 'rgb(102, 102, 102)'
-            }
-          },
-          autotick: false,
-          dtick: 10,
-          ticks: 'outside',
-          tickcolor: 'rgb(102, 102, 102)'
+            showgrid: false,
+            showline: true,
+            linecolor: 'rgb(102, 102, 102)',
+            titlefont: {
+                font: {
+                    color: 'rgb(204, 204, 204)'
+                }
+            },
+            tickfont: {
+                font: {
+                    color: 'rgb(102, 102, 102)'
+                }
+            },
+            autotick: false,
+            dtick: 10,
+            ticks: 'outside',
+            tickcolor: 'rgb(102, 102, 102)'
         },
         margin: {
-          l: 140,
-          r: 40,
-          b: 50,
-          t: 80
+            l: 140,
+            r: 40,
+            b: 50,
+            t: 80
         },
         legend: {
-          font: {
-            size: 10,
-          },
-          yanchor: 'middle',
-          xanchor: 'right'
+            font: {
+                size: 10,
+            },
+            yanchor: 'middle',
+            xanchor: 'right'
         },
         width: 600,
         height: 600,
         paper_bgcolor: 'rgb(254, 247, 234)',
         plot_bgcolor: 'rgb(254, 247, 234)',
         hovermode: 'closest'
-      };
-      
-      return Plotly.newPlot("myGraph", graphtrace, layout);
+    };
+
+    return Plotly.newPlot("myGraph", graphtrace, layout);
 };
